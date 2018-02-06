682c6dc02cd61fd4f743e3369f37a610852707ca
Rodrigo <rodrigodot@outlook.com>
2018-02-05 23:41:48 -0200

    App working with hello

--- index.android.js
+++ index.android.js
@@ -1,53 +1,14 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+var React = require('react');
+var Text = require('react-native').Text;
+var AppRegistry = require('react-native').AppRegistry;
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+const App = function() {
 
-export default class app1 extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+  return (
+    <Text>Hello Rodrigo</Text>
+  );
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+AppRegistry.registerComponent('app1', function() {
+  return App
 });
-
-AppRegistry.registerComponent('app1', () => app1);
