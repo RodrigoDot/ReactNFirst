44afca223ad4ae6e5082298929a6eaa5c8252dfb
Rodrigo <rodrigodot@outlook.com>
2018-02-06 00:04:53 -0200

    Update Arrow functions

--- index.android.ES5.js
+++ index.android.ES5.js
@@ -2,10 +2,12 @@ var React = require('react');
 var Text = require('react-native').Text;
 var AppRegistry = require('react-native').AppRegistry;
 
-const App = () => {
+const App = function() {
   return (
     <Text>Hello Rodrigo</Text>
   );
 };
 
-AppRegistry.registerComponent('app1', () => App);
+AppRegistry.registerComponent('app1', function() {
+  return App
+});
